769d0f2ca208734555010f0462c42d68ba7d9001
AkakiryuLatias <AkakiryuLatias@users.noreply.github.com>
2017-01-01 12:11:46 -1000

    ZPD cards up
    All of the ZPD cards added

---
+++ ZPD/script/c90000109.lua
@@ -0,0 +1,81 @@
+-- ＺＰＤ警察 − 「ビャクロウ」
+function c90000109.initial_effect(c)
+	--direct attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_DIRECT_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(c90000109.datg)
+	c:RegisterEffect(e1)
+	--damage reduce
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+	e2:SetCondition(c90000109.rdcon)
+	e2:SetOperation(c90000109.rdop)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x4b0))
+	c:RegisterEffect(e2)
+	--atkup
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetDescription(aux.Stringid(90000109,1))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetHintTiming(TIMING_DAMAGE_STEP)
+	e3:SetRange(LOCATION_HAND)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetCondition(c90000109.condition2)
+	e3:SetCost(c90000109.cost2)
+	e3:SetOperation(c90000109.operation2)
+	c:RegisterEffect(e3)
+end
+
+--direct attack
+function c90000109.filter(e,c)
+	return c:IsSetCard(0x4b0)
+end
+
+function c90000109.datg(e,c)
+	return c:IsSetCard(0x4b0)
+end
+function c90000109.rdcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return ep~=tp and c==Duel.GetAttacker() and Duel.GetAttackTarget()==nil
+		and c:GetEffectCount(EFFECT_DIRECT_ATTACK)<2 and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
+end
+function c90000109.rdop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChangeBattleDamage(ep,ev/2)
+end
+
+--Honest
+function c90000109.condition2(e,tp,eg,ep,ev,re,r,rp)
+	local phase=Duel.GetCurrentPhase()
+	if phase~=PHASE_DAMAGE or Duel.IsDamageCalculated() then return false end
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return (d~=nil and a:GetControler()==tp and a:IsSetCard(0x4b0) and a:IsRelateToBattle())
+		or (d~=nil and d:GetControler()==tp and d:IsFaceup() and d:IsSetCard(0x4b0) and d:IsRelateToBattle())
+end
+function c90000109.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+end
+function c90000109.operation2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if not a:IsRelateToBattle() or not d:IsRelateToBattle() then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetOwnerPlayer(tp)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+	if a:GetControler()==tp then
+		e1:SetValue(d:GetAttack())
+		a:RegisterEffect(e1)
+	else
+		e1:SetValue(a:GetAttack())
+		d:RegisterEffect(e1)
+	end
+end
